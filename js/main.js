76ba85b522d6bbfdd7952656722e26a2066908f9
Shane McElroy <mcelroysh@gmail.com>
2025-07-06 12:16:27 -0700

    Implement master calculation function
    Added a new function that accepts two numbers and
    an operator that calls on one of the math functions.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+let firstNumber = 10;
+let secondNumber = 5;
+let operator = "+";
+
+function operate(n1, n2, operator) {
+  switch (operator) {
+    case "+":
+      return add(n1, n2);
+    case "-":
+      return subtract(n1, n2);
+    case "*":
+      return multiply(n1, n2);
+    case "/":
+      return divide(n1, n2);
+    default:
+      return "Invalid Operator.";
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
