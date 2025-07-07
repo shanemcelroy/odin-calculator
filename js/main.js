8663a8993b80917c4a31e3e459e42f7b5ab7bbb4
Shane McElroy <mcelroysh@gmail.com>
2025-07-06 17:49:48 -0700

    Populate screen on button press
    The calculator screen displays the input from the user
    and clears the screen with the clear button.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,26 @@ let firstNumber = 10;
 let secondNumber = 5;
 let operator = "+";
 
+const buttons = document.querySelector(".btn-container");
+const display = document.querySelector(".screen-text");
+const reset = document.querySelector(".clear");
+
+buttons.addEventListener("click", (e) => {
+  let button = e.target;
+  let value = button.textContent;
+
+  display.textContent = value;
+});
+
+reset.addEventListener("click", (e) => {
+  e.stopPropagation();
+  clearScreen();
+});
+
+function clearScreen() {
+  display.textContent = "";
+}
+
 function operate(n1, n2, operator) {
   switch (operator) {
     case "+":
